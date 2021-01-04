08ec0a0240cc7248da241d36ecd0c517e5de3dd7
Tina <1098299287@qq.com>
2021-01-04 10:55:26 +0800

    这是一个实现了请求CSS JS HTML XML JSON的AJAX

---
+++ public/main.js
@@ -0,0 +1,100 @@
+let n = 1;
+
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/${n + 1}.json`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const array = JSON.parse(request.response);
+      array.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+      n += 1;
+    }
+  };
+  request.send();
+};
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const object = JSON.parse(request.response);
+      console.log(object);
+      myName.textContent = object.name;
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const dom = request.responseXML;
+        console.log(dom);
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text.trim());
+      }
+    }
+  };
+  request.send();
+};
+
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement("div");
+        div.innerHTML = request.response;
+        document.body.appendChild(div);
+      } else {
+        alert("加载失败");
+      }
+    }
+  };
+  request.send();
+};
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const script = document.createElement("script");
+        script.innerHTML = request.response;
+        document.body.appendChild(script);
+      } else {
+        alert("加载失败");
+      }
+    }
+  };
+  request.send();
+};
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "style.css");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载失败");
+      }
+    }
+  };
+  request.send();
+};
